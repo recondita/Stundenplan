ca712ce6ece40846ee43bb3e2c92c2e56664ec1b
felix <felix@familie-schuetze.de>
2013-09-07 01:38:06 +0200

    GUI erweitert sich dynamisch [BETA]

--- src/GUILehrer.java
+++ src/GUILehrer.java
@@ -5,22 +5,22 @@ import java.awt.event.*;
 
 public class GUILehrer extends JScrollPane
 {
-	JPanel panel=new JPanel();
+	JPanel panel = new JPanel();
 	Datenbank db = new Datenbank();
-	int fach_anzahl=(db.gebeFaecherListe().length);
+	int fach_anzahl = (db.gebeFaecherListe().length);
 	Strings sT;
 	JLabel beschreibung[] = new JLabel[4];
 	JTextField eingabe[] = new JTextField[3];
-	JTextField[] eingabe_fach=new JTextField[fach_anzahl];
-	JPanel[] p =new JPanel[fach_anzahl];
+	JTextField[] eingabe_fach = new JTextField[fach_anzahl];
+	JPanel[] p = new JPanel[fach_anzahl];
 	JLabel ausgabe = new JLabel();
 	JButton start;
-	JPanel unten=new JPanel();
-	JPanel mitte=new JPanel();
-	JPanel oben=new JPanel();
+	JPanel unten = new JPanel();
+	JPanel mitte = new JPanel();
+	JPanel oben = new JPanel();
 	JComboBox[] auswahl = new JComboBox[fach_anzahl];
 	String Fach_liste = "";
-	int anzahl=0;
+	int anzahl = 0;
 
 	/**
 	 * @author: Felix Schütze
@@ -28,18 +28,18 @@ public class GUILehrer extends JScrollPane
 	public GUILehrer(Strings sT)
 	{
 		setViewportView(panel);
-		p[0]=new JPanel();
+		p[0] = new JPanel();
 		sT = new Strings();
 		start = new JButton(sT.speichern);
 		panel.setLayout(new BorderLayout());
-		oben.setLayout(new GridLayout(3,2));
-		unten.setLayout(new GridLayout(1,1));
-		unten.setPreferredSize(new Dimension(25,25));
-		mitte.setLayout(new GridLayout(anzahl+1,2));
-		oben.setPreferredSize(new Dimension(100,100));
-		panel.add("North",(oben));
-		panel.add("Center",(mitte));
-		panel.add("South",(unten));
+		oben.setLayout(new GridLayout(3, 2));
+		unten.setLayout(new GridLayout(1, 1));
+		unten.setPreferredSize(new Dimension(25, 25));
+		mitte.setLayout(new GridLayout(anzahl + 1, 2));
+		oben.setPreferredSize(new Dimension(100, 100));
+		panel.add("North", (oben));
+		panel.add("Center", (mitte));
+		panel.add("South", (unten));
 		p[0].setLayout(new GridLayout(1, 2));
 		for (int i = 0; i < 3; i++)
 		{
@@ -51,7 +51,7 @@ public class GUILehrer extends JScrollPane
 		beschreibung[3] = new JLabel();
 		eingabe_fach[0] = new JTextField();
 		mitte.add(beschreibung[3]);
-		auswahl[0]=new JComboBox();
+		auswahl[0] = new JComboBox();
 		p[0].add(auswahl[0]);
 		mitte.add(p[0]);
 		p[0].add(eingabe_fach[0]);
@@ -77,20 +77,21 @@ public class GUILehrer extends JScrollPane
 
 	public void dropDown()
 	{
-		for(int j=0;j<anzahl+1;j++)
-		{
 		auswahl[anzahl].addItemListener(new ItemListener()
 		{
 			public void itemStateChanged(ItemEvent e)
 			{
 				JComboBox selectedChoice = (JComboBox) e.getSource();
-				eingabe_fach[anzahl].setText(""+selectedChoice.getSelectedItem());
+				eingabe_fach[anzahl].setText(""
+						+ selectedChoice.getSelectedItem());
 				anzahl++;
+				mitte.setLayout(new GridLayout(anzahl + 1, 2));
 				mitte.add(new JPanel());
+				p[anzahl] = new JPanel();
 				mitte.add(p[anzahl]);
 				p[anzahl].setLayout(new GridLayout(1, 2));
-				auswahl[anzahl]=new JComboBox();
-				eingabe_fach[anzahl]=new JTextField();
+				auswahl[anzahl] = new JComboBox();
+				eingabe_fach[anzahl] = new JTextField();
 				for (int i = 0; i < fach_anzahl; i++)
 				{
 					auswahl[anzahl].addItem(db.gebeFaecherListe()[i]);
@@ -100,7 +101,6 @@ public class GUILehrer extends JScrollPane
 				validate();
 			}
 		});
-		}
 	}
 
 	public void listener()
@@ -112,10 +112,13 @@ public class GUILehrer extends JScrollPane
 			{
 				if (!eingabe[0].getText().equals("")
 						&& !eingabe[1].getText().equals("")
-						&& !eingabe[2].getText().equals("")
-						&& !eingabe[3].getText().equals(""))
+						&& !eingabe[2].getText().equals(""))
 				{
-					String[] temp = eingabe[3].getText().split("\\, ");
+					String[] temp=new String[anzahl+1];
+					for (int i = 0; i < anzahl + 1; i++)
+					{
+						temp[i] = eingabe_fach[i].getText();
+					}
 					db.schreibeLehrerEigeschaften(eingabe[0].getText(),
 							Integer.parseInt(eingabe[1].getText()),
 							Integer.parseInt(eingabe[2].getText()), temp);
