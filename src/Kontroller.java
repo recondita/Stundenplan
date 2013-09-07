ebb2bbf6b0eeaded3c3c27e3bb15c434d0be37d3
Jan <jan.hofmeier@yahoo.de>
2013-09-07 02:22:44 +0200

    Kein Plan

--- src/Kontroller.java
+++ src/Kontroller.java
@@ -2,10 +2,40 @@
 public class Kontroller 
 {
 	GUI gUI;
-	Faecher fAECHER=new Faecher();
-	public Kontroller(GUI gUI)
+	Lehrer[] lehrer;
+	Klasse[] klasse;
+	
+
+	
+	
+	public Kontroller(GUI gUI, Lehrer[] lehrer, Klasse[] klassen)
 	{
 		this.gUI=gUI;
+		this.lehrer=lehrer;
+		this.klasse=klassen;
+		//fach=new int[5*10*lehrer.length*klasse.length][5][10][lehrer.length][klasse.length];
 	}
 	
+	public void generieren()
+	{
+		// [klasse][Wochentag][Stude]=fach;
+		//boolean [][][][][] fach = new boolean[5][10][lehrer.length][klasse.length];
+	}
+
+	public void Einzeplan()	
+	{
+		for(int tag=0; tage<5; tage++)
+		{
+			for(int stunde=0; stunde<10; stunde++)
+			{
+				
+			}
+		}
+	}
+	
+	public int[] tag(int stunde, int fach)
+	
+	
 }
+
+
