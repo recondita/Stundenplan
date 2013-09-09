792f3f494edf7020a004429223664d1ea76e2c4e
Jan <jan.hofmeier@yahoo.de>
2013-09-09 14:39:42 +0200

    Methode für einen Zufallstundenpplan für eine Klasse begonnen

--- src/Generator.java
+++ src/Generator.java
@@ -2,21 +2,23 @@ public class Generator
 {
 	GUI gUI;
 	Lehrer[] lehrer;
-	Klasse[][] klasse;
+	Klasse[][] klassen;
+	Fach[] faecher;
 	
 	//Lehrer,Tag,Stunde
 	boolean [][][] lehrerFrei;
 	int [][] prioritaetLehrer;
 	
 	
-	public Generator(GUI gUI, Lehrer[] lehrer, Klasse[][] klasse, Fach[] Fach)
+	public Generator(GUI gUI, Lehrer[] lehrer, Klasse[][] klasse, Fach[] fach)
 	{
 		this.gUI=gUI;
 		this.lehrer=lehrer;
-		this.klasse=klasse;
+		this.klassen=klasse;
+		this.faecher=fach;
 		//fach=new int[5*10*lehrer.length*klasse.length][5][10][lehrer.length][klasse.length];
-		lehrerFrei=new boolean[lehrer.length][5][10];
-		prioritaetLehrer= new int[klasse.length][lehrer.length];
+		//lehrerFrei=new boolean[lehrer.length][5][10];
+		//prioritaetLehrer= new int[klasse.length][lehrer.length];
 	}
 	
 	public void generieren()
@@ -25,4 +27,34 @@ public class Generator
 		//boolean [][][][][] fach = new boolean[5][10][lehrer.length][klasse.length];
 	}
 
+	
+	private Stundenplan generiereZufallsPlan(int stufe, int klasse)
+	{
+		int [] fachStunden= klassen[stufe][klasse].fachStunden.clone();
+		int[][] stunden=new int[10][5];
+		for (int i=0; i< stunden.length; i++)
+		{
+			for(int j=0; i<stunden[j].length; j++)
+			{
+				int[]fachIndex= new int [faecher.length];
+				int verfuegbar=0;
+				for(int k=0; k<fachStunden.length;k++)
+				{
+					if(fachStunden[k]>0)
+					{
+						fachIndex[verfuegbar]=k;
+						verfuegbar++;
+					}
+				}
+				if(verfuegbar>0)
+				{
+				int fach=fachIndex[(int)Math.random()*verfuegbar];
+				stunden[i][j]=fach;
+				fachStunden[fach]--;				
+			}
+		}
+		
+		
+		return null;
+	}
 }
